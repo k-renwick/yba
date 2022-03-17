c9c5be8a5a8d5fadf825bc8ad9173c041a8744de
Katrina <krenwick@outlook.com>
2022-03-16 21:23:24 -0400

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,81 @@
 import logo from "./logo.png";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "./App.css";
+
+const darkTheme = createTheme({
+  palette: { mode: "dark", primary: { main: "#1976d2" } },
+});
+
+const pages = [
+  {
+    key: "aboutUs",
+    name: "About Us",
+    uri: "/about-us",
+    element: <AboutUs />,
+  },
+  {
+    key: "services",
+    name: "Services",
+    uri: "/services",
+    element: <Services />,
+  },
+  {
+    key: "getInvolved",
+    name: "Get Involved",
+    uri: "/get-involved",
+    element: <GetInvolved />,
+  },
+  {
+    key: "fixYourOwnBike",
+    name: "Fix Your Own Damn Bike!",
+    uri: "/fix-your-own-bike",
+    element: <FixYourOwnBike />,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div class="viewport">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Website coming soon!</p>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <NavBar pages={pages} />
+        <div className="viewport">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Website coming soon!</p>
 
-        <Routes>
-          <Route path="/" element={<AboutUs />} />
-          <Route path="about" element={<AboutUs />} />
-          <Route path="services" element={<Services />} />
-          <Route path="get-involved" element={<GetInvolved />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<AboutUs />} />
+            {pages.map((page) => (
+              <Route path={page.uri} element={page.element} />
+            ))}
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
 function AboutUs() {
   return (
     <div>
-      <h2>About</h2>
+      <h2>About Us</h2>
+      <ul>
+        <li>History</li>
+        <li>Location</li>
+        <li>Hours</li>
+        <li>Meet the team</li>
+        <li>Community Involvement</li>
+      </ul>
+    </div>
+  );
+}
+
+function FixYourOwnBike() {
+  return (
+    <div>
+      <h2>Fix Your Own Damn Bike!</h2>
     </div>
   );
 }
@@ -35,6 +84,13 @@ function Services() {
   return (
     <div>
       <h2>Services</h2>
+      <ul>
+        <li>Kids bikes</li>
+        <li>Refurbishing</li>
+        <li>Repairs</li>
+        <li>Bike Skills Education</li>
+        <li>Recycling</li>
+      </ul>
     </div>
   );
 }
@@ -43,6 +99,12 @@ function GetInvolved() {
   return (
     <div>
       <h2>Get Involved</h2>
+      <ul>
+        <li>Donate</li>
+        <li>Recycle old bikes or parts</li>
+        <li>Volunteer</li>
+        <li>Become a sponsor</li>
+      </ul>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,74 +9,72 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Link } from "react-router-dom";
 import logo from "./../logo.png";
 
-const darkTheme = createTheme({
-  palette: { mode: "dark", primary: { main: "#1976d2" } },
-});
-
-const pages = ["About Us", "Services", "Get Involved"];
-
-const ResponsiveAppBar = () => {
+const NavBar = ({ pages }) => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const handleOpenNavMenu = (e) => setAnchorElNav(e.currentTarget);
   const handleCloseNavMenu = () => setAnchorElNav(null);
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <AppBar position="static">
-        <Container maxWidth={false}>
-          <Toolbar disableGutters>
-            <img src={logo} className="yba-logo" alt="logo" />
+    <AppBar position="static">
+      <Container maxWidth={false}>
+        <Toolbar disableGutters>
+          <img src={logo} className="yba-logo" alt="logo" />
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton size="large" onClick={handleOpenNavMenu}>
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                keepMounted
-                transformOrigin={{ vertical: "top", horizontal: "left" }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: "block", md: "none" } }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton size="large" onClick={handleOpenNavMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+              keepMounted
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
             >
-              Yellow Bike Action
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
-                <Button
-                  key={page}
-                  size="large"
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 1, color: "white", display: "block" }}
-                >
-                  {page}
-                </Button>
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link className="nav-item" to={page.uri}>
+                      {page.name}
+                    </Link>
+                  </Typography>
+                </MenuItem>
               ))}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </ThemeProvider>
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            Yellow Bike Action
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                size="large"
+                onClick={handleCloseNavMenu}
+                sx={{ my: 1, color: "white", display: "block" }}
+              >
+                <Link className="nav-item" to={page.uri}>
+                  {page.name}
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
-export default ResponsiveAppBar;
+export default NavBar;
